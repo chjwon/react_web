353f41031867b49d4c87eb1fd10a309ab3ac3225
Jae Won Choi <chjwon10044@gmail.com>
2022-11-15 19:42:24 -0600

    fix model func

--- xai/Diabetes_Health_Indicators/model.py
+++ xai/Diabetes_Health_Indicators/model.py
@@ -1,17 +1,18 @@
 from foldrm import *
 from getCSV import diabete_binary_5050split
-def dh_model():
+def dh_model(inputData):
     model, data = diabete_binary_5050split()
     data_train, data_test = split_data(data, ratio=0.8)
     model.fit(data_train, ratio=0.5)
     model.print_asp(simple=True)
-
     tempData=[0.0,1.0,0.0,1.0,26.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0,3.0,5.0,30.0,0.0,1.0,4.0,6.0,8.0]
-    print(model.explain(tempData))
-    print(model.proof(tempData))
+    
+    inputData = tempData
+
     temp = model.proof(tempData)
     tempIndex = temp.index("is")
     print(temp[tempIndex+3])
+    # 0 = no diabetes 1 = prediabetes 2 = diabetes
     return temp[tempIndex+3]
 
 dh_model()
--- xai/Heart_Disease/model.py
+++ xai/Heart_Disease/model.py
@@ -1,28 +1,17 @@
 from foldrm import *
 from getCSV import heart_disease
 
-def hd_model():
+
+def hd_model(inputData):
     model, data = heart_disease()
     data_train, data_test = split_data(data, ratio=0.8)
     model.fit(data_train, ratio=0.5)
-
-
     model.print_asp(simple=True)
-    # Y = [d[-1] for d in data_test]
-    # Y_test_hat = model.predict(data_test)
-    # acc = get_scores(Y_test_hat, data_test)
-    # print('% acc', round(acc, 4), '# rules', len(model.crs))
-    # acc, p, r, f1 = scores(Y_test_hat, Y, weighted=True)
-    # print('% acc', round(acc, 4), 'macro p r f1', round(p, 4), round(r, 4), round(f1, 4), '# rules', len(model.crs))
-
-
     tempData = [52,1,0,125,212,0,1,168,0,1,2,2,3,0]
-    # print("11111")
-    # print(type(model.explain(tempData)))
-    # print(model.explain(tempData))
 
-    # print(model.proof(tempData))
-    temp = model.proof(tempData)
+    inputData = tempData
+    
+    temp = model.proof(inputData)
     tempIndex = temp.index("is")
     print("val of thal is:",temp[tempIndex+3])
     # thal is thal: 0 = normal; 1 = fixed defect; 2 = reversable defect
